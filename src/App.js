fed98b852c472d206ef3f0983494162b81211e4d
lnalice <rara9915@gmail.com>
2023-12-07 01:05:40 +0900

    Add initial React-Project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Notice from './pages/notice/Notice';
+import NoticeDetail from './pages/notice/NoticeDetail';
+import Problem from './pages/problem/Problem';
+import Company from './pages/problem/Company';
+import Level from './pages/problem/Level';
+import Type from './pages/problem/Type';
+import Header from './components/Header'
+
+function App() {
+
+    return (
+            <BrowserRouter>
+            <Header />
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/notice" element={<Notice />}></Route>
+                    <Route path="/notice/detail/:id" element={<NoticeDetail />}></Route>
+                    <Route path="/problem" element={<Problem />}></Route>
+                    <Route path="/problem/company" element={<Company />}></Route>
+                    <Route path="/problem/type" element={<Type />}></Route>
+                    <Route path="/problem/level" element={<Level />}></Route>
+                </Routes>
+            </BrowserRouter>
+    );
+}
+
+export default App;
