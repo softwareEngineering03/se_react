a9ed91614f00ee8ffbc431983e8a555bfc4e32ac
lnalice <rara9915@gmail.com>
2023-12-07 04:01:53 +0900

    Add 유형별 문제 목록

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './pages/Main';
 import Notice from './pages/notice/Notice';
 import NoticeDetail from './pages/notice/NoticeDetail';
 import Problem from './pages/problem/Problem';
+import ProblemByType from './pages/problem/ProblemByType';
 import Company from './pages/problem/Company';
 import Level from './pages/problem/Level';
 import Type from './pages/problem/Type';
@@ -19,6 +20,7 @@ function App() {
                     <Route path="/notice" element={<Notice />}></Route>
                     <Route path="/notice/detail/:id" element={<NoticeDetail />}></Route>
                     <Route path="/problem" element={<Problem />}></Route>
+                    <Route path="/problemByType" element={<ProblemByType />}></Route>
                     <Route path="/problem/company" element={<Company />}></Route>
                     <Route path="/problem/type" element={<Type />}></Route>
                     <Route path="/problem/level" element={<Level />}></Route>
